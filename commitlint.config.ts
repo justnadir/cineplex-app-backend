0605f3c65e8b6948ff368ef6b14a07c9cd91631e
Nadir Hossain <nadirhossain336@gmail.com>
2026-08-02 15:32:24 +0600

    chore: add commitlint and husky git hooks
    - Add commit-msg hook to enforce conventional commit format
    - Add pre-commit hook for pre-commit checks
    - Configure commitlint with commitlint.config.ts
    - Update package.json with new dev dependencies and scripts
    - Update tsconfig.json for commitlint config compatibility
    - Update yarn.lock accordingly

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+import type { UserConfig } from "@commitlint/types";
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "build",
+        "ci",
+        "revert",
+      ],
+    ],
+    "subject-case": [0],
+  },
+};
+
+export default config;
